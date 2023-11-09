b80fb8d3decee1b097baeef3bd7be03df609ac6a
Rudra Joshi <rudrajoshi2481@gmail.com~>
2023-11-09 23:47:41 +0530

    update 01

--- components/ScrollAnimation.tsx
+++ components/ScrollAnimation.tsx
@@ -64,8 +64,8 @@ const FramerBox = chakra(motion.div, {
      */
     return (
       <Box className="parallax">
-        <AnimatePresence>
-        <FramerBox    exit={{ opacity: 0 }} key="modal" className="scroller" style={{ x }}>
+        <AnimatePresence mode='wait'  initial={false}>
+        <FramerBox     key="modal" className="scroller" style={{ x }}>
           <span className={NotoFonts.className}>{children} </span>
           <span className={NotoFonts.className}>{children} </span>
           <span className={NotoFonts.className}>{children} </span>
